a4df469f632cffbdb2be26f54f4f4d7d3f4d25f7
Harrison Saperstein <harrison.saperstein@gmail.com>
2019-04-02 00:14:13 +0100

    added text and worked on modals css

--- javascript/modals.js
+++ javascript/modals.js
@@ -1,6 +1,6 @@
 function open_modal(index, id){
     document.getElementById(id).style.display='block';
-    document.getElementsByClassName("tab")[index-1].click()
+    document.getElementsByClassName("modal_tablink")[index-1].click()
 }
 function close_modal(id){
     document.getElementById(id).style.display='none';
@@ -12,11 +12,13 @@ function open_question(event, question_number) {
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    tabs = document.getElementsByClassName("tab");
+    tabs = document.getElementsByClassName("modal_tablink");
     for (i = 0; i < x.length; i++) {
-        tabs[i].classList.remove("tab");
+        tabs[i].classList.remove("w3-light-grey");
     }
     document.getElementById(question_number).style.display = "block";
+    event.currentTarget.classList.add("w3-light-grey");
+
 }
 
 function run_curriculum(){}
