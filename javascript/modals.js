55b9af668eb10e1392207b2fd47e483dd29cd350
Harrison Saperstein <harrison.saperstein@gmail.com>
2019-04-26 00:24:03 +0100

    work on ajax

--- javascript/modals.js
+++ javascript/modals.js
@@ -1,24 +1,25 @@
 function open_modal(index, id){
     document.getElementById(id).style.display='block';
-    document.getElementsByClassName("modal_tablink")[index-1].click()
+    document.getElementsByClassName("tablink")[index-1].click()
 }
 function close_modal(id){
     document.getElementById(id).style.display='none';
 }
 
-function open_question(event, question_number) {
-    let i, x, tabs;
+function open_question(evt, questionNumber) {
+    var i, x, tablinks;
     x = document.getElementsByClassName("question");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    tabs = document.getElementsByClassName("modal_tablink");
+    tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < x.length; i++) {
-        tabs[i].classList.remove("w3-light-grey");
+        tablinks[i].classList.remove("w3-light-grey");
     }
-    document.getElementById(question_number).style.display = "block";
-    event.currentTarget.classList.add("w3-light-grey");
-
+    document.getElementById(questionNumber).style.display = "block";
+    evt.currentTarget.classList.add("w3-light-grey");
 }
+
+
 
 function run_curriculum(){}
